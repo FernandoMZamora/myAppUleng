79c99c12a30647ca1cf5ce07d372b6cb1b956544
Renyerj <Pochote>
2019-03-18 16:46:29 -0600

    Creacion pag a

---
+++ src/app/a/a.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-a',
+  templateUrl: './a.page.html',
+  styleUrls: ['./a.page.scss'],
+})
+export class APage{
+
+  constructor(private router: Router) { }
+
+  home(){
+    this.router.navigate(["/home"]);
+  }
+
+  regresar_categoria(){
+    this.router.navigate(["/categorias"]);
+  }
+
+  siguiente(){
+    this.router.navigate(["/b"]);
+  }
+}
